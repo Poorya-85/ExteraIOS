558ea7a7a01a95cb3effde78f94dbd626be91530
Poorya <140714703+Poorya-85@users.noreply.github.com>
2026-09-01 22:22:48 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,19 @@
 pluginManagement {
     repositories {
-        gradlePluginPortal()
         google()
         mavenCentral()
-        maven("[https://api.xposed.info/](https://api.xposed.info/)")
+        gradlePluginPortal()
     }
-}
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven("https://api.xposed.info/")
     }
 }
 
-rootProject.name = "XposedModuleTemplate"
+rootProject.name = "ExteraIOS"
 include(":app")
-includeBuild("libxposed-api")
-include(":libxposed-compat")
